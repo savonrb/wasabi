02435e96197b3ce2f549d77806838b59976ba0e1
rubiii <me@rubiii.com>
2012-07-20 12:06:47 +0200

    added medium-size wsdl for performance testing

---
+++ spec/wasabi/sax_parser/economic_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe Wasabi::SAXParser do
+
+  subject(:sax) { Wasabi::SAXParser.new }
+
+  context "with economic.wsdl" do
+    before do
+      st = Time.now
+      parse(:economic)
+      et = Time.now
+      puts "time: #{et - st}"
+    end
+
+    it "knows the target namespace" do
+      expect(sax.target_namespace).to eq("http://e-conomic.com")
+    end
+  end
+
+  def parse(fixture_name)
+    parser = Nokogiri::XML::SAX::Parser.new(sax)
+    parser.parse fixture(fixture_name).read
+  end
+
+end
