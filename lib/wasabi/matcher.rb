bcb104765f5b4ff017a19a3a64217345f43e8d33
rubiii <me@rubiii.com>
2012-07-20 13:38:54 +0200

    reduced iterations to improve performance

--- lib/wasabi/matcher.rb
+++ lib/wasabi/matcher.rb
@@ -9,7 +9,7 @@ module Wasabi
       return false if @matcher.size != stack.size
 
       @matcher.each_with_index do |nodes, index|
-        return false unless nodes.include?(stack[index])
+        return false unless nodes.include?(stack[-(index+1)])
       end
       true
     end
@@ -17,13 +17,16 @@ module Wasabi
     private
 
     def parse(matcher)
-      nodes = matcher.split(" > ")
-      nodes.each_with_index do |node, index|
-        nsids, local = node.split(":")
-        nsids = nsids.split("|")
-        nodes[index] = nsids.map { |nsid| "#{nsid}:#{local}" }
-      end
-      nodes
+      nodes = matcher.split(" > ").reverse
+      nodes.map { |node|
+        if node.include?("|")
+          nsids, local = node.split(":")
+          nsids = nsids.split("|")
+          nsids.map { |nsid| "#{nsid}:#{local}" }
+        else
+          [node]
+        end
+      }
     end
 
   end
