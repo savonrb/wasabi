b39ec1f77252c37200d130105d7a5652f9f04681
rubiii <me@rubiii.com>
2013-01-13 20:51:34 +0100

    removed duplicated spec helpers

--- spec/wasabi/sax_parser/betfair_spec.rb
+++ spec/wasabi/sax_parser/betfair_spec.rb
@@ -1,24 +1,9 @@
 require "spec_helper"
 
 describe Wasabi::SAX do
+  include SpecSupport::SAX
 
   subject(:sax) { new_sax(:betfair).hash }
-
-  def count_elements(element_type)
-    sax[:schemas].inject(0) { |count, schema|
-      count + schema[element_type].count
-    }
-  end
-
-  def find_element(element_type, element_name)
-    all_elements(element_type)[element_name]
-  end
-
-  def all_elements(element_type)
-    sax[:schemas].inject({}) { |memo, schema|
-      memo.merge schema[element_type]
-    }
-  end
 
   context "with betfair.wsdl" do
     it "knows the simple types" do
