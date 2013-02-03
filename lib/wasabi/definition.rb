ba3d8a23d3e2e25f9b374897c2218bcb797e8203
rubiii <me@rubiii.com>
2013-02-03 18:07:55 +0100

    moved type code from Interface to Definition

--- lib/wasabi/definition.rb
+++ lib/wasabi/definition.rb
@@ -43,6 +43,54 @@ module Wasabi
       binding_operation["soap_action"].to_s
     end
 
+    def types
+      @types = {}
+
+      @definition[:schemas].each do |schema|
+        schema[:elements].each do |element_name, element|
+          complex_type = element["complexType"]
+          process_type(element_name, schema, complex_type) if complex_type
+        end
+
+        schema[:complex_types].each do |complex_type_name, complex_type|
+          process_type(complex_type_name, schema, complex_type)
+        end
+      end
+
+      resolve_deferred_types
+      @types
+    end
+
+    def deferred_types
+      @deferred_types ||= []
+    end
+
+    def resolve_deferred_types
+      deferred_types.each(&:call)
+    end
+
+    def type_map
+      type_map = {}
+
+      @definition[:schemas].each do |schema|
+        schema[:elements].each do |name, type|
+          if complex_type = type["complexType"]
+            type_map_element(type_map, name, schema, complex_type)
+          end
+        end
+
+        schema[:complex_types].each do |name, type|
+          type_map_element(type_map, name, schema, type)
+        end
+
+        schema[:simple_types].each do |name, type|
+          type_map_element(type_map, name, schema, type)
+        end
+      end
+
+      type_map
+    end
+
     private
 
     def endpoints!
@@ -144,6 +192,104 @@ module Wasabi
 
     def raise_port_type_error!(binding)
       raise DefinitionError, "Unable to find port type for binding:\n" + binding.inspect
+    end
+
+    def process_type(name, schema, type)
+      @types[name] ||= { :namespace => schema[:namespace] }
+
+      if type["sequence"] && type["sequence"]["element"]
+        type["sequence"]["element"].each do |element|
+          @types[name][element["name"]] = { :type => element["type"] }
+        end
+      elsif type["complexContent"] && type["complexContent"]["extension"]
+        extension  = type["complexContent"]["extension"]
+        elements   = extension["sequence"] && extension["sequence"]["element"]
+        elements ||= []
+
+        elements.each do |element|
+          @types[name][element["name"]] = { :type => element["type"] }
+        end
+
+        if extension["base"]
+          base = extension["base"].match(/\w+$/).to_s
+
+          if @types[base]
+            @types[name].merge! @types[base]
+          else
+            deferred_types << Proc.new { @types[name].merge! @types[base] }
+          end
+        end
+      end
+    end
+
+    def type_map_element(type_map, name, schema, type)
+      type_map[name] ||= type_element = {}  # { :namespace => schema[:namespace] }
+
+      #
+      # { :sequence => [
+      #   {
+      #     "minOccurs" => "1",
+      #     "maxOccurs" => "1",
+      #     "name"      => "CreditCardType",
+      #     "type"      => "tns:CCType"
+      #   }
+      # ]}
+      #
+      if type["sequence"]
+        sequence = type["sequence"]["element"]
+        type_element[:sequence] = sequence
+
+      #
+      # { :extension => "ReturnValue",          # optional
+      #   :sequence  => [                       # optional
+      #     {
+      #       "minOccurs" => "1",
+      #       "maxOccurs" => "1",
+      #       "name"      => "CreditCardType",
+      #       "type"      => "tns:CCType"
+      #     }
+      #   ]
+      # }
+      #
+      elsif type["complexContent"] && type["complexContent"]["extension"]
+        extension = type["complexContent"]["extension"]
+
+        if extension["base"]
+          base = extension["base"].match(/\w+$/).to_s
+          type_element[:extension] = base
+        end
+
+        if extension["sequence"]
+          elements = extension["sequence"]["element"]
+          type_element[:sequence] = elements
+        end
+
+        other_keys = extension.keys - %w(base sequence)
+        raise "other keys!" unless other_keys.empty?
+
+      #
+      # { :restriction => {
+      #   :base        => "xsd:string",
+      #   :enumeration => ["OK", "API_ERROR"]
+      # }}
+      #
+      elsif type["restriction"]
+        type_element.merge! type
+
+      #
+      # { :empty => true }
+      #
+      elsif type.empty?
+        type_element[:empty] = true
+
+      elsif type["annotation"]
+        nil  # ignore
+
+      else
+        raise "else!"
+      end
+
+      type_map[name] = type_element
     end
 
   end
--- lib/wasabi/interface.rb
+++ lib/wasabi/interface.rb
@@ -52,46 +52,15 @@ module Wasabi
       @operations ||= operations!
     end
 
-    def operations!
-      operations = {}
-
-      definition.operations.each do |operation_name, _|
-        operations[operation_name] = {
-          :input       => definition.input_for(operation_name),
-          :output      => definition.output_for(operation_name),
-          :soap_action => definition.soap_action_for(operation_name)
-        }
-      end
-
-      operations
-    end
-
     def type_map
-      @type_map ||= type_map!
+      @type_map ||= definition.type_map
     end
 
     def types
-      @types = {}
-
-      definition.definition[:schemas].each do |schema|
-        schema[:elements].each do |element_name, element|
-          complex_type = element["complexType"]
-          process_type(element_name, schema, complex_type) if complex_type
-        end
-
-        schema[:complex_types].each do |complex_type_name, complex_type|
-          process_type(complex_type_name, schema, complex_type)
-        end
-      end
-
-      resolve_deferred_types
-      @types
+      @types ||= definition.types
     end
 
-    def deferred_types
-      @deferred_types ||= []
-    end
-
+    # XXX: legacy method. convert specs and remove! [dh, 2013-02-03]
     def type_namespaces
       namespaces = []
 
@@ -105,6 +74,7 @@ module Wasabi
       namespaces
     end
 
+    # XXX: legacy method. convert specs and remove! [dh, 2013-02-03]
     def type_definitions
       result = []
 
@@ -122,6 +92,7 @@ module Wasabi
       result
     end
 
+    # XXX: legacy method. convert specs and remove! [dh, 2013-02-03]
     def user_defined?(namespace)
       uri = namespaces["xmlns:#{namespace}"]
       !(uri =~ %r{^http://schemas.xmlsoap.org} || uri =~ %r{^http://www.w3.org})
@@ -129,135 +100,25 @@ module Wasabi
 
     private
 
+    def operations!
+      operations = {}
+
+      definition.operations.each do |operation_name, _|
+        operations[operation_name] = {
+          :input       => definition.input_for(operation_name),
+          :output      => definition.output_for(operation_name),
+          :soap_action => definition.soap_action_for(operation_name)
+        }
+      end
+
+      operations
+    end
+
     def definition
        @definition ||= begin
          parser = Wasabi.parser(@source, @http_request)
          Wasabi.definition(parser.definition)
        end
-    end
-
-    def process_type(name, schema, type)
-      @types[name] ||= { :namespace => schema[:namespace] }
-
-      if type["sequence"] && type["sequence"]["element"]
-        type["sequence"]["element"].each do |element|
-          @types[name][element["name"]] = { :type => element["type"] }
-        end
-      elsif type["complexContent"] && type["complexContent"]["extension"]
-        extension  = type["complexContent"]["extension"]
-        elements   = extension["sequence"] && extension["sequence"]["element"]
-        elements ||= []
-
-        elements.each do |element|
-          @types[name][element["name"]] = { :type => element["type"] }
-        end
-
-        if extension["base"]
-          base = extension["base"].match(/\w+$/).to_s
-
-          if @types[base]
-            @types[name].merge! @types[base]
-          else
-            deferred_types << Proc.new { @types[name].merge! @types[base] }
-          end
-        end
-      end
-    end
-
-    def resolve_deferred_types
-      deferred_types.each(&:call)
-    end
-
-    def type_map!
-      type_map = {}
-
-      definition.definition[:schemas].each do |schema|
-        schema[:elements].each do |name, type|
-          if complex_type = type["complexType"]
-            type_map_element(type_map, name, schema, complex_type)
-          end
-        end
-
-        schema[:complex_types].each do |name, type|
-          type_map_element(type_map, name, schema, type)
-        end
-
-        schema[:simple_types].each do |name, type|
-          type_map_element(type_map, name, schema, type)
-        end
-      end
-
-      type_map
-    end
-
-    def type_map_element(type_map, name, schema, type)
-      type_map[name] ||= type_element = {}  # { :namespace => schema[:namespace] }
-
-      #
-      # { :sequence => [
-      #   {
-      #     "minOccurs" => "1",
-      #     "maxOccurs" => "1",
-      #     "name"      => "CreditCardType",
-      #     "type"      => "tns:CCType"
-      #   }
-      # ]}
-      #
-      if type["sequence"]
-        sequence = type["sequence"]["element"]
-        type_element[:sequence] = sequence
-
-      #
-      # { :extension => "ReturnValue",          # optional
-      #   :sequence  => [                       # optional
-      #     {
-      #       "minOccurs" => "1",
-      #       "maxOccurs" => "1",
-      #       "name"      => "CreditCardType",
-      #       "type"      => "tns:CCType"
-      #     }
-      #   ]
-      # }
-      #
-      elsif type["complexContent"] && type["complexContent"]["extension"]
-        extension = type["complexContent"]["extension"]
-
-        if extension["base"]
-          base = extension["base"].match(/\w+$/).to_s
-          type_element[:extension] = base
-        end
-
-        if extension["sequence"]
-          elements = extension["sequence"]["element"]
-          type_element[:sequence] = elements
-        end
-
-        other_keys = extension.keys - %w(base sequence)
-        raise "other keys!" unless other_keys.empty?
-
-      #
-      # { :restriction => {
-      #   :base        => "xsd:string",
-      #   :enumeration => ["OK", "API_ERROR"]
-      # }}
-      #
-      elsif type["restriction"]
-        type_element.merge! type
-
-      #
-      # { :empty => true }
-      #
-      elsif type.empty?
-        type_element[:empty] = true
-
-      elsif type["annotation"]
-        nil  # ignore
-
-      else
-        raise "else!"
-      end
-
-      type_map[name] = type_element
     end
 
   end
