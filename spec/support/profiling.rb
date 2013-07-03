929c618742189c661922ca80e238d2ca4226f3a6
rubiii <me@rubiii.com>
2013-07-03 21:58:39 +0200

    only load profiling libs when they're needed

--- spec/support/profiling.rb
+++ spec/support/profiling.rb
@@ -1,14 +1,15 @@
-require 'benchmark'
-require 'method_profiler'
-
 module SpecSupport
 
   def benchmark(&block)
+    require 'benchmark'
+
     puts "Benchmark:"
     puts Benchmark.measure(&block)
   end
 
   def profile_methods
+    require 'method_profiler'
+
     profiler = MethodProfiler.observe(Wasabi::Parser)
     yield
     puts profiler.report
