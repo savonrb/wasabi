e1ff3650851e89f793b6cb96a5ce5d5c2de38f05
Oleg Kukareka <oleg@kukareka.com>
2015-05-18 23:39:33 +0300

    Convert spaces to underlines

--- lib/wasabi/core_ext/string.rb
+++ lib/wasabi/core_ext/string.rb
@@ -8,6 +8,7 @@ module Wasabi
         str.gsub!(/::/, '/')
         str.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
         str.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+        str.gsub!(/\s+/, "_")
         str.tr! ".", "_"
         str.tr! "-", "_"
         str.downcase!
