d7ec6227f100ae3e891c81d845318d4f50d94075
Olle Jonsson <olle.jonsson@gmail.com>
2020-07-15 20:19:38 +0200

    Use RSpec 3.7 (#82)
    * Update RSpec version
    * Remove warning: 'Using `stub` from rspec-mocks' old `:should` syntax without explicitly enabling the syntax is deprecated. Use the new `:expect` syntax'
    * Avoid RSpec 3 deprecation warnings
    
    Co-authored-by: Chaymae Bazzaoui <bazzaoui.chaymae@gmail.com>
    Co-authored-by: Bruno Vicenzo <bruno@vicenzo.me>

--- spec/wasabi/document/multiple_namespaces_spec.rb
+++ spec/wasabi/document/multiple_namespaces_spec.rb
@@ -7,17 +7,17 @@ describe Wasabi::Document do
 
     describe '#namespace' do
       subject { super().namespace }
-      it { should == "http://example.com/actions" }
+      it { is_expected.to eq "http://example.com/actions" }
     end
 
     describe '#endpoint' do
       subject { super().endpoint }
-      it { should == URI("http://example.com:1234/soap") }
+      it { is_expected.to eq URI("http://example.com:1234/soap") }
     end
 
     describe '#element_form_default' do
       subject { super().element_form_default }
-      it { should == :qualified }
+      it { is_expected.to eq :qualified }
     end
 
     it 'has 1 operation' do
@@ -27,28 +27,28 @@ describe Wasabi::Document do
     describe '#operations' do
       subject { super().operations }
       it do
-      should == { :save => { :input => "Save", :output=>"SaveResponse", :action => "http://example.com/actions.Save", :namespace_identifier => "actions", :parameters => { :article => { :name => "article", :type => "Article" } } } }
-    end
+        is_expected.to match({ :save => { :input => "Save", :output=>"SaveResponse", :action => "http://example.com/actions.Save", :namespace_identifier => "actions", :parameters => { :article => { :name => "article", :type => "Article" } } } })
+      end
     end
 
     describe '#type_namespaces' do
       subject { super().type_namespaces }
       it do
-      should =~ [
-        [["Save"], "http://example.com/actions"],
-        [["Save", "article"], "http://example.com/actions"],
-        [["Article"], "http://example.com/article"],
-        [["Article", "Author"], "http://example.com/article"],
-        [["Article", "Title"], "http://example.com/article"]
-      ]
-    end
+        is_expected.to match([
+          [["Save"], "http://example.com/actions"],
+          [["Save", "article"], "http://example.com/actions"],
+          [["Article"], "http://example.com/article"],
+          [["Article", "Author"], "http://example.com/article"],
+          [["Article", "Title"], "http://example.com/article"]
+        ])
+      end
     end
 
     describe '#type_definitions' do
       subject { super().type_definitions }
       it do
-      should =~ [ [["Save", "article"], "Article"] ]
-    end
+        is_expected.to match([ [["Save", "article"], "Article"] ])
+      end
     end
 
   end
