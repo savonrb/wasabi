fbdb54845b56aad4084a635ad99435b3ae750bab
Архипов Дмитрий <da@netcitylife.ru>
2023-11-24 19:31:37 +0400

    Improve parser to return operation faults

--- spec/wasabi/parser/workday_spec.rb
+++ spec/wasabi/parser/workday_spec.rb
@@ -15,5 +15,9 @@ describe Wasabi::Parser do
     it "parses the operations" do
       expect(subject.operations[:get_customer_invoices][:input]).to eq("Get_Customer_Invoices_Request")
     end
+
+    it "returns faults" do
+      expect(subject.operations[:get_customer_invoices][:fault]).to eq(["Validation_Fault", "Processing_Fault"])
+    end
   end
 end
