savonrb/wasabi

commit 299a671980348a2d525495ff33378010aa25a6c6

Parse Input/Output When Operation Style is Document (#114)

lib/wasabi/parser.rb
@@ -144,8 +144,9 @@ module Wasabi
         # TODO: check for soap namespace?
         soap_operation = operation.element_children.find { |node| node.name == 'operation' }
         soap_action = soap_operation['soapAction'] if soap_operation
+        soap_document = soap_operation["style"] if soap_operation
 
-        if soap_action
+        if soap_action || soap_document
           soap_action = soap_action.to_s
           action = soap_action && !soap_action.empty? ? soap_action : name
 
@@ -247,8 +248,7 @@ module Wasabi
         port_type_operation = @port_type_operations[binding_type][operation_name]
       end
 
-      port_type_input_output = port_type_operation &&
-        port_type_operation.element_children.find { |node| node.name == input_output }
+      port_type_input_output = port_type_operation&.element_children&.find { |node| node.name == input_output }
 
       # TODO: Stupid fix for missing support for imports.
       # Sometimes portTypes are actually included in a separate WSDL.
@@ -260,6 +260,8 @@ module Wasabi
         end
 
         message_ns_id, message_type = nil
+        message_ns_id = port_message_ns_id
+        message_type = port_message_type
 
         # When there is a parts attribute in soap:body element, we should use that value
         # to look up the message part from messages array.
@@ -270,8 +272,8 @@ module Wasabi
         end
 
         message = @messages[port_message_type]
-        port_message_part = message.element_children.find do |node|
-          soap_body_parts.nil? ? (node.name == 'part') : ( node.name == 'part' && node['name'] == soap_body_parts)
+        port_message_part = message&.element_children&.find do |node|
+          soap_body_parts.nil? ? (node.name == "part") : (node.name == "part" && node["name"] == soap_body_parts)
         end
 
         if port_message_part && port_element = port_message_part.attribute('element')

spec/wasabi/document/geotrust_spec.rb
@@ -30,9 +30,35 @@ describe Wasabi::Document do
       subject { super().operations }
       it do
         should include(
-          { :get_quick_approver_list => { :input => "GetQuickApproverList", :action => "GetQuickApproverList", :parameters=>{:Request=>{:name=>"Request", :type=>"GetQuickApproverListInput"}}}},
-          { :hello => { :input => "hello", :action => "hello", :parameters=>{:Input=>{:name=>"Input", :type=>"string"}} } }
-          )
+          {
+            get_quick_approver_list: {
+              input: "GetQuickApproverList",
+              output: "GetQuickApproverListResponse",
+              action: "GetQuickApproverList",
+              namespace_identifier: "s1",
+              parameters: {
+                Request: {
+                  name: "Request",
+                  type: "GetQuickApproverListInput"
+                }
+              }
+            }
+          },
+          {
+            hello: {
+              input: "hello",
+              output: "helloResponse",
+              action: "hello",
+              namespace_identifier: "s1",
+              parameters: {
+                Input: {
+                  name: "Input",
+                  type: "string"
+                }
+              }
+            }
+          }
+        )
       end
     end
 

spec/wasabi/document/savon295_spec.rb
@@ -11,7 +11,14 @@ describe Wasabi::Document do
       subject { super().operations }
       it do
       should include(
-        { :sendsms => { :input => "sendsms", :output => "sendsms", :action => "sendsms", :namespace_identifier => "tns" } }
+          {
+            sendsms: {
+              input: "sendsmsRequest",
+              output: "sendsmsResponse",
+              action: "sendsms",
+              namespace_identifier: "tns"
+            }
+          }
       )
     end
     end

spec/wasabi/parser/no_message_parts_spec.rb
@@ -15,7 +15,7 @@ describe Wasabi::Parser do
 
     it 'falls back to using the message type in the port element' do
       # Operation's input has no part element in the message, so using the message type.
-      expect(subject.operations[:save][:input]).to eq('Save')
+      expect(subject.operations[:save][:input]).to eq("SaveSoapIn")
 
       # Operation's output has part element in the message, so using part element's type.
       expect(subject.operations[:save][:output]).to eq('SaveResponse')
@@ -26,7 +26,7 @@ describe Wasabi::Parser do
     end
 
     it 'gracefully handles port messages without a colon' do
-      expect(subject.operations[:delete][:input]).to eq('Delete')
+      expect(subject.operations[:delete][:input]).to eq("DeleteSoapIn")
       expect(subject.operations[:delete][:output]).to eq('DeleteResponse')
       expect(subject.operations[:delete][:namespace_identifier]).to be_nil
     end

spec/wasabi/parser/tradetracker_spec.rb
@@ -13,7 +13,7 @@ describe Wasabi::Parser do
     let(:xml) { fixture(:tradetracker).read }
 
     it 'parses the operations' do
-      expect(subject.operations[:get_feeds][:input]).to eq('getFeeds')
+      expect(subject.operations[:get_feeds][:input]).to eq("GetFeedsMessage")
     end
   end
 end

spec/wasabi/document/namespaced_actions_spec.rb
@@ -29,12 +29,33 @@ describe Wasabi::Document do
     describe '#operations' do
       subject { super().operations }
       it do
-      should include(
-        { :delete_client => { :input => "Client.Delete", :output => "Client.DeleteResponse", :action => "http://api.example.com/api/Client.Delete", :namespace_identifier => "tns" } },
-        { :get_clients   => { :input => "User.GetClients", :output => "User.GetClientsResponse", :action => "http://api.example.com/api/User.GetClients", :namespace_identifier => "tns" } },
-        { :get_api_key   => { :input => "User.GetApiKey", :output => "User.GetApiKeyResponse", :action => "http://api.example.com/api/User.GetApiKey", :namespace_identifier => "tns" } }
-      )
-    end
+        should include(
+          {
+            delete_client: {
+              input: "Client.Delete",
+              output: "Client.DeleteResponse",
+              action: "http://api.example.com/api/Client.Delete",
+              namespace_identifier: "tns"
+            }
+          },
+          {
+            get_clients: {
+              input: "User.GetClients",
+              output: "User.GetClientsResponse",
+              action: "http://api.example.com/api/User.GetClients",
+              namespace_identifier: "tns"
+            }
+          },
+          {
+            get_api_key: {
+              input: "User.GetApiKey",
+              output: "User.GetApiKeyResponse",
+              action: "http://api.example.com/api/User.GetApiKey",
+              namespace_identifier: "tns"
+            }
+          }
+        )
+      end
     end
 
   end

spec/wasabi/document/no_namespace_spec.rb
@@ -29,12 +29,33 @@ describe Wasabi::Document do
     describe '#operations' do
       subject { super().operations }
       it do
-      should include(
-        { :get_user_login_by_id => { :input => "GetUserLoginById", :output => "GetUserLoginById", :action => "/api/api/GetUserLoginById", :namespace_identifier => "typens" } },
-        { :get_all_contacts => { :input => "GetAllContacts", :output =>"GetAllContacts", :action => "/api/api/GetAllContacts", :namespace_identifier => "typens" } },
-        { :search_user => { :input => "SearchUser", :output =>"SearchUser", :action => "/api/api/SearchUser", :namespace_identifier => nil } }
-      )
-    end
+        should include(
+          {
+            get_user_login_by_id: {
+              input: "GetUserLoginById",
+              output: "GetUserLoginByIdResponse",
+              action: "/api/api/GetUserLoginById",
+              namespace_identifier: "typens"
+            }
+          },
+          {
+            get_all_contacts: {
+              input: "GetAllContacts",
+              output: "GetAllContactsResponse",
+              action: "/api/api/GetAllContacts",
+              namespace_identifier: "typens"
+            }
+          },
+          {
+            search_user: {
+              input: "SearchUser",
+              output: "SearchUserResponse",
+              action: "/api/api/SearchUser",
+              namespace_identifier: nil
+            }
+          }
+        )
+      end
     end
 
   end

commit 1a7bb934232660cc6bfdbe04dd849db52d9b71c4

Adjust input parsing (#122)
Fixes a regression introduced in #114

lib/wasabi/parser.rb
@@ -239,6 +239,7 @@ module Wasabi
       input_output_for(operation, 'output')
     end
 
+    # @return [namespace_id, message_type]
     def input_output_for(operation, input_output)
       operation_name = operation['name']
 
@@ -251,9 +252,13 @@ module Wasabi
 
       port_type_input_output = port_type_operation&.element_children&.find { |node| node.name == input_output }
 
+      # find the message for the portType operation
+      # if there is no message, we will use the operation name as the message name
+
       # TODO: Stupid fix for missing support for imports.
       # Sometimes portTypes are actually included in a separate WSDL.
       if port_type_input_output
+        # If the message attribute contains a colon, it means the message is namespaced.
         if port_type_input_output.attribute('message').to_s.include? ':'
           port_message_ns_id, port_message_type = port_type_input_output.attribute('message').to_s.split(':')
         else
@@ -261,13 +266,6 @@ module Wasabi
         end
 
         message_ns_id, message_type = nil
-
-        soap_operation = operation.element_children.find { |node| node.name == 'operation' }
-
-        if soap_operation.nil? || soap_operation['style'] != 'rpc'
-          message_ns_id = port_message_ns_id
-          message_type = port_message_type
-        end
 
         # When there is a parts attribute in soap:body element, we should use that value
         # to look up the message part from messages array.
@@ -277,6 +275,7 @@ module Wasabi
           soap_body_parts = soap_body_element['parts'] if soap_body_element
         end
 
+        # look for any message part that matches the soap body parts
         message = @messages[port_message_type]
         port_message_part = message&.element_children&.find do |node|
           soap_body_parts.nil? ? (node.name == "part") : (node.name == "part" && node["name"] == soap_body_parts)
@@ -288,6 +287,22 @@ module Wasabi
             message_ns_id, message_type = port_message_part.split(':')
           else
             message_type = port_message_part
+          end
+        end
+
+        # If the message is not found, we should use the operation name as the message name for document style operations
+        # applies only to output
+        if input_output == 'output'
+          # if the operation is document style and theres no port_message_part, we should use the operation_name
+          soap_operation = operation.element_children.find { |node| node.name == 'operation' }
+          if message_type.nil? && (soap_operation.nil? || soap_operation['style'] != 'rpc')
+            if port_message_part.nil?
+              message_ns_id = port_message_ns_id
+              message_type = operation_name
+            else
+              message_ns_id = port_message_ns_id
+              message_type = port_message_type
+            end
           end
         end
 

spec/wasabi/document/geotrust_spec.rb
@@ -5,35 +5,35 @@ require "spec_helper"
 describe Wasabi::Document do
   context "with: geotrust.wsdl" do
 
-    subject { Wasabi::Document.new fixture(:geotrust).read }
+    let(:document) { Wasabi::Document.new(fixture(:geotrust).read) }
 
     describe '#namespace' do
-      subject { super().namespace }
-      it { should == "http://api.geotrust.com/webtrust/query" }
+      subject { document.namespace }
+      it { should eq "http://api.geotrust.com/webtrust/query" }
     end
 
     describe '#endpoint' do
-      subject { super().endpoint }
-      it { should == URI("https://test-api.geotrust.com:443/webtrust/query.jws") }
+      subject { document.endpoint }
+      it { should eq URI("https://test-api.geotrust.com:443/webtrust/query.jws") }
     end
 
     describe '#element_form_default' do
-      subject { super().element_form_default }
-      it { should == :qualified }
+      subject { document.element_form_default }
+      it { should be :qualified }
     end
 
     it 'has 2 operations' do
-      expect(subject.operations.size).to eq(2)
+      expect(document.operations.size).to eq(2)
     end
 
     describe '#operations' do
-      subject { super().operations }
+      subject { document.operations }
       it do
         should include(
           {
             get_quick_approver_list: {
               input: "GetQuickApproverList",
-              output: "GetQuickApproverListResponse",
+              output: "GetQuickApproverList",
               action: "GetQuickApproverList",
               namespace_identifier: "s1",
               parameters: {

spec/wasabi/document/savon295_spec.rb
@@ -13,7 +13,7 @@ describe Wasabi::Document do
       should include(
           {
             sendsms: {
-              input: "sendsmsRequest",
+              input: "sendsms",
               output: "sendsmsResponse",
               action: "sendsms",
               namespace_identifier: "tns"

spec/wasabi/parser/no_message_parts_spec.rb
@@ -15,7 +15,7 @@ describe Wasabi::Parser do
 
     it 'falls back to using the message type in the port element' do
       # Operation's input has no part element in the message, so using the message type.
-      expect(subject.operations[:save][:input]).to eq("SaveSoapIn")
+      expect(subject.operations[:save][:input]).to eq("Save")
 
       # Operation's output has part element in the message, so using part element's type.
       expect(subject.operations[:save][:output]).to eq('SaveResponse')
@@ -26,7 +26,7 @@ describe Wasabi::Parser do
     end
 
     it 'gracefully handles port messages without a colon' do
-      expect(subject.operations[:delete][:input]).to eq("DeleteSoapIn")
+      expect(subject.operations[:delete][:input]).to eq("Delete")
       expect(subject.operations[:delete][:output]).to eq('DeleteResponse')
       expect(subject.operations[:delete][:namespace_identifier]).to be_nil
     end

spec/wasabi/parser/tradetracker_spec.rb
@@ -13,7 +13,7 @@ describe Wasabi::Parser do
     let(:xml) { fixture(:tradetracker).read }
 
     it 'parses the operations' do
-      expect(subject.operations[:get_feeds][:input]).to eq("GetFeedsMessage")
+      expect(subject.operations[:get_feeds][:input]).to eq("getFeeds")
     end
   end
 end