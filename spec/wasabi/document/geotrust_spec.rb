8b6b2a1aa265b71ae0653dcf8a036af822548d41
Архипов Дмитрий <da@netcitylife.ru>
2026-07-07 10:54:28 +0200

    Add faults parsing, return original name in operation hash

--- spec/wasabi/document/geotrust_spec.rb
+++ spec/wasabi/document/geotrust_spec.rb
@@ -31,6 +31,7 @@ describe Wasabi::Document do
         should include(
           {
             get_quick_approver_list: {
+              name: "GetQuickApproverList",
               input: "GetQuickApproverList",
               output: "GetQuickApproverList",
               action: "GetQuickApproverList",
@@ -45,6 +46,7 @@ describe Wasabi::Document do
           },
           {
             hello: {
+              name: "hello",
               input: "hello",
               output: "helloResponse",
               action: "hello",
